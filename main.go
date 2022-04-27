03e18000a59e48a2d4f384d9c7a08f2421095715
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-04-27 13:09:34 +0430

    basic details

--- main.go
+++ main.go
@@ -5,3 +5,23 @@ import "fmt"
 func main() {
 	fmt.Println("hello world")
 }
+
+// if we type go run or go build hello_world in terminal ,out put :
+
+//hello_world
+
+// if we type ls , output :
+
+//hello_world  hello_world.go
+
+// if we type ./hello_world ,output :
+
+//hello world
+
+//more details :
+
+//To start a program, a name must always be placed as the name of the package at the beginning of the file,
+// such as the main package. You can use different names for Choose the name of your package,
+//but the main file on which the executable command is issued must be the main package name.
+//Note that when the program starts, only the code that we put in the main function will be executed,
+// so it is better to use our code as Write the function outside it and call inside the main function.
