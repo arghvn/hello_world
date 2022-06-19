c39ac985376cc21d462d7115ffbe547c1ab13cdc
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-19 17:30:44 +0430

    out put

--- Regular_Expressions.go
+++ Regular_Expressions.go
@@ -41,3 +41,19 @@ func main() {
 	out := r.ReplaceAllFunc(in, bytes.ToUpper)
 	fmt.Println(string(out))
 }
+
+// output :
+
+// true
+// true
+// peach
+// idx: [0 5]
+// [peach ea]
+// [0 5 1 3]
+// [peach punch pinch]
+// all: [[0 5 1 3] [6 11 7 9] [12 17 13 15]]
+// [peach punch]
+// true
+// regexp: p([a-z]+)ch
+// a <fruit>
+// a PEACH
