045280bdf9d31f83d6f77ff8d81698c15584c6ef
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-19 17:35:54 +0430

    comments

--- Regular_Expressions.go
+++ Regular_Expressions.go
@@ -1,5 +1,7 @@
 package main
 
+// note
+// Edit this code from the source
 import (
 	"bytes"
 	"fmt"
@@ -8,6 +10,8 @@ import (
 
 func main() {
 
+	// Go offers built-in support for regular expressions.
+	//  Here are some examples of common regexp-related tasks in Go.This tests whether a pattern matches a string.
 	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
 	fmt.Println(match)
 
@@ -16,20 +20,23 @@ func main() {
 	fmt.Println(r.MatchString("peach"))
 
 	fmt.Println(r.FindString("peach punch"))
-
+	// Above we used a string pattern directly, but for other regexp tasks you’ll need to Compile an optimized Regexp struct.
+	// This finds the match for the regexp.
 	fmt.Println("idx:", r.FindStringIndex("peach punch"))
-
+	// This also finds the first match but returns the start and end indexes for the match instead of the matching text.
 	fmt.Println(r.FindStringSubmatch("peach punch"))
-
+	// The Submatch variants include information about both the whole-pattern matches and the submatches within those matches.
+	// For example this will return information for both p([a-z]+)ch and ([a-z]+).
 	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
 
 	fmt.Println(r.FindAllString("peach punch pinch", -1))
-
+	// Similarly this will return information about the indexes of matches and submatches.
 	fmt.Println("all:", r.FindAllStringSubmatchIndex(
 		"peach punch pinch", -1))
-
+	// The All variants of these functions apply to all matches in the input, not just the first.
+	// For example to find all matches for a regexp.
 	fmt.Println(r.FindAllString("peach punch pinch", 2))
-
+	// These All variants are available for the other functions we saw above as well.
 	fmt.Println(r.Match([]byte("peach")))
 
 	r = regexp.MustCompile("p([a-z]+)ch")
